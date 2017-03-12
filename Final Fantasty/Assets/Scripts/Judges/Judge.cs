f837d0777d6718cf33ab0638089066eb80cf75cc
RevRocky <rocky@weeklybeat.org>
2017-03-12 11:59:59 -0400

    Calculate Stats method and Judge level init

--- Final Fantasty/Assets/Scripts/Judges/Judge.cs
+++ Final Fantasty/Assets/Scripts/Judges/Judge.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Judge : MonoBehaviour {
-
 	
-	public int CalStats(){
-
+	public const int NUM_STATS = 6;
+	public string name;
+	public byte[] statModifiers;
+	
+	// Initialisation Function
+	public void init(byte[] statModifiers, string name) {
+		this.statModifiers = statModifiers;	// Assigning a judges stat mods
+		this.name = name;
+	}
+	
+	// Computes a weighted sum of stats. Card stats is the a given players'
+	// card while modifiers is the overpowering flavour modifiers from
+	// their opponents submission;
+	public int CalculateStats(byte[] cardStats, byte[] modifiers){
+		// Initialise i and sum
+		int i = 0;
+		int sum = 0;
+		
+		for(; i < NUM_STATS; i++) {
+			sum += statModifiers[i] * (cardStats[i] + modifiers[i]);
+		}
 	}
 
-	public void Talk() {
+	public abstract void Talk() {
 		
 	}
 	
