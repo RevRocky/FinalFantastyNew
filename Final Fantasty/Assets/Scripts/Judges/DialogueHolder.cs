b941412dc3992a04f9bc3b8c4323fa3c6a50e35d
Katherine Le <katherinele@uyens-MacBook-Pro.local>
2017-03-12 01:40:00 -0500

    Judge scene

---
+++ Final Fantasty/Assets/Scripts/Judges/DialogueHolder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour {
+
+	public string dialogue;
+	private DialogueManager dMan;
+	public string[] dialogueLines;
+
+	// Use this for initialization
+	void Start () {
+		dMan = FindObjectOfType<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			//dMan.ShowBox(dialogue);
+			if (!dMan.dialogActive)
+			{
+				dMan.dialogLines = dialogueLines;
+				dMan.ShowDialogues();
+			}
+		}
+		
+	}
+
+}
