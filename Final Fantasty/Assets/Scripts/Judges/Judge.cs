ae2e980ccb7797433b98e2bdfb643602989cc064
Katherine Le <katherinele@uyens-MacBook-Pro.local>
2017-03-12 11:48:20 -0400

    Beta1

--- Final Fantasty/Assets/Scripts/Judges/Judge.cs
+++ Final Fantasty/Assets/Scripts/Judges/Judge.cs
@@ -5,23 +5,7 @@ using UnityEngine.UI;
 
 public class Judge : MonoBehaviour {
 
-	public struct PlayerSubmission {
-	public string owner;				// Whose card is this
-	public string cardTitle;			// Name of submitted card
-	public byte[] stats;				// Card stats
-	public byte[] overpoweringMods;		// Modifiers for overpowering flavour
-	public Sprite cardImage;			// Incase you want a picture
-
-	// Fairly Straight Forward Constructor
-	public PlayerSubmission(string owner, string cardTitle, byte[] stats, byte[] overpoweringMods, Sprite image) {
-		this.owner = owner;
-		this.cardTitle = cardTitle;
-		this.stats = stats;
-		this.overpoweringMods = overpoweringMods;
-		this.cardImage = image;
-		}
-	}
-
+	
 	public int CalStats(){
 
 	}
