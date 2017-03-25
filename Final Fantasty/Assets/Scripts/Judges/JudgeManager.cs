c5be6913fe39e1ddab39ef0214922a23b89d91b4
Katherine Le <14ttul@queensu.ca>
2017-03-25 10:38:46 -0400

    dynamically generate judge

--- Final Fantasty/Assets/Scripts/Judges/JudgeManager.cs
+++ Final Fantasty/Assets/Scripts/Judges/JudgeManager.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class JudgeManager : MonoBehaviour {
 
 	public static JudgeManager instance = null;
-	public string[] judgeCats = {"sweet", "sour", "spicy", "salty", "unamy"};
-	public Judge[] judges;		// Judges one and two known to the player
-	Judge judgeTwo;
-	private int currentIndex = 0;
+	private GenerateJudges genJ;
 
 
 	// Use this for initialization
@@ -20,46 +17,11 @@ public class JudgeManager : MonoBehaviour {
 			Destroy(gameObject);
 
 		DontDestroyOnLoad(gameObject);
-		RandomJudge();
+		genJ = FindObjectOfType<GenerateJudges>();
+
 
 	
 		
-	}
-	//Choose a random judge script---not complete
-	private void RandomJudge(){
-		judges = new Judge[3];
-		int randJudge;
-		for (int i = 0; i < 3; i++) {
-			randJudge = 0;		// Should be random. For testing purposes! 
-			switch (randJudge) {
-				case(0):
-					Sweet newSweet = gameObject.AddComponent<Sweet>() as Sweet;
-					newSweet.init();
-					judges[i] = (Judge) newSweet;
-				break;
-				case(1):
-					Sour newSour = gameObject.AddComponent<Sour>() as Sour;
-					newSour.init();
-					judges[i] = (Judge) newSour;
-					break;
-				case(2):
-					Spicy newSpicy = gameObject.AddComponent<Spicy>() as Spicy;
-					newSpicy.init();
-					judges[i] = (Judge) newSpicy;
-					break;
-				case(3):
-					Salty newSalty = gameObject.AddComponent<Salty>() as Salty;
-					newSalty.init();
-					judges[i] = (Judge) newSalty;
-					break;
-				case(4):
-					Unamy newJudge = gameObject.AddComponent<Unamy>() as Unamy;
-					newJudge.init();
-					judges[i] = (Judge) newJudge;
-					break;
-
-			}
-		}
 	}
 
 	
